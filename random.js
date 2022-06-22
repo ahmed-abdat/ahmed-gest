40f34d846c4758272ef25dfb48b0551b6fa346a7
ahmed-abdat <ahmedeabdat@gmail.com>
2022-06-22 00:16:02 +0000

    ups date

--- random.js
+++ random.js
@@ -1,17 +1,10 @@
 let rendom = [];
-<<<<<<< HEAD
+
 let btn = document.getElementById("btn");
 let start = document.getElementById("rand");
 let inp = document.getElementById("name");
 let show = document.getElementById("show");
-=======
-var k = 1;
-do {
-  var name = prompt("أدخل إسمك ؟ أو إضغض موافق للخروج");
-  if (!(name === "null")) {
-    rendom.push(name);
-  }
->>>>>>> 89a9996798080cf41b5cfa42f509089772abb7c6
+
 
 
 
@@ -32,19 +25,7 @@ console.log(rendom);
 let result = start.onclick = function(){
   let randomNumber = Math.floor(Math.random() * rendom.length);
 let randomName = rendom[randomNumber];
-<<<<<<< HEAD
 show.innerHTML = `🎉 والفائز هو ${randomName}`;
-=======
-console.log(randomNumber);
-if (rendom.length) {
-  if (k) {
-    let container = `<h1 class="content">
-     <span> 🎉${randomName} </span>: و الفائز هو
-      </h1>`;
-
-    document.write(container);
-  }
->>>>>>> 89a9996798080cf41b5cfa42f509089772abb7c6
 }
 
 
