91a6bb4364cdd1474a50f55114dca6ad52d50868
ahmed-abdat <ahmedeabdat@gmail.com>
2022-06-22 10:52:43 +0000

    up dates

--- random.js
+++ random.js
@@ -5,28 +5,17 @@ let start = document.getElementById("rand");
 let inp = document.getElementById("name");
 let show = document.getElementById("show");
 
-
-
-
-
-let ran = btn.onclick = function () {
+let ran = (btn.onclick = function () {
   if (inp.value != "") {
     rendom.push(inp.value);
     console.log(rendom);
     inp.value = "";
   }
-};
+});
 console.log(rendom);
 
-
-
-
-
-let result = start.onclick = function(){
+let result = (start.onclick = function () {
   let randomNumber = Math.floor(Math.random() * rendom.length);
-let randomName = rendom[randomNumber];
-show.innerHTML = `🎉 والفائز هو ${randomName}`;
-}
-
-
-
+  let randomName = rendom[randomNumber];
+  show.innerHTML = ` والفائز هو ${randomName} 🎉`;
+});
